cb6d40f1c9e75cc934596a148146a2c5c7fdb6b7
andytrung195 <78588150+andytrung195@users.noreply.github.com>
2021-11-01 10:46:31 +0700

    adding Chat form and Geolocation

---
+++ server/utils/message.test.js
@@ -0,0 +1,27 @@
+let expect = require("expect");
+
+let { generateMessage, generateLocationMessage } = require("./message");
+
+describe("Generate Message", () => {
+  it("should generate correct message object", () => {
+    let from = "WDJ",
+      text = "Some random text",
+      message = generateMessage(from, text);
+
+    expect(typeof message.createdAt).toBe("number");
+    expect(message).toMatchObject({ from, text });
+  });
+});
+
+describe('Generate Location Message', ()=>{
+  it('should generate correct location object', ()=> {
+    let from = 'Trung',
+        lat = 12;
+        lng = 100;
+        url = `https://www.google.com/maps?q=${lat}, ${lng}`,
+        message = generateLocationMessage(from, lat, lng);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, url})
+  })
+})
